f97c64a85258dd50277a93d406c16da42972f448
Nima Azaraeen <nima.azaraeen@gmail.com>
2020-04-29 06:12:17 +0400

    index.html serve support added to server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
