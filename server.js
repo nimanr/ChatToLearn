66d094e56458890f4acaa71401ec60c8f06837c4
Nima Azaraeen <nima.azaraeen@gmail.com>
2020-04-29 06:20:56 +0400

    Get request and handling from both sides written

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+let messages = [
+  { name: "nima", message: "hello" },
+  { name: "nima", message: "hollo everyone" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
