8cda6c7bc2f328b587c1b4143b5641e0c7a2ef27
Nima Azaraeen <nima.azaraeen@gmail.com>
2020-04-29 06:34:11 +0400

    POST request and handling from both sides written

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-let messages = [
-  { name: "nima", message: "hello" },
-  { name: "nima", message: "hollo everyone" },
-];
+let messages = [];
 
 app.get("/messages", (req, res) => {
   res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  const incoming = req.body;
+  messages.push({ name: incoming.name, message: incoming.message });
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
